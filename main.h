eccd110413ee889efb8dc36489719c08e23135e9
Sanctus-Peter <sanctuspeter@gmail.com>
2022-08-06 15:40:42 +0100

    Updaed header file

--- main.h
+++ main.h
@@ -10,6 +10,11 @@ int _printf(const char *, ...);
 void signedNumberToString(int64_t, int, char *, char *);
 void unsignedNumberToString(int64_t, int, char *, char *);
 int _putchar(char);
-int print_string(char *);
+int print_string(char *, int *, int);
+char *rot13(char *);
+int get_index(char *, char);
+int isDigit(char);
+int isAlpha(char);
+int setFlags(char *, int *, va_list);
 
 #endif
