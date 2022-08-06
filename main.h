59312a7f3746f213eb11d5772c1d07fcf6bdb383
Sanctus-Peter <sanctuspeter@gmail.com>
2022-08-06 15:04:48 +0100

    Updated Header file

--- main.h
+++ main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _printf(const char *format, ...);
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdint.h>
+
+int _printf(const char *, ...);
+void signedNumberToString(int64_t, int, char *, char *);
+void unsignedNumberToString(int64_t, int, char *, char *);
+int _putchar(char);
+int print_string(char *);
 
 #endif
