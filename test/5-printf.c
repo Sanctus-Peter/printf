da31b693c2695b5d7c825f73c61be8b4dd6031d7
mamt4real <mamt4real@gmail.com>
2022-08-09 07:34:29 -0700

    test -1

--- test/5-printf.c
+++ test/5-printf.c
@@ -19,7 +19,7 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 	if (format[0] == '%' && format[1] == '\0')
-		return (0);
+		return (-1);
 	val = _vprintf(format, args);
 	va_end(args);
 
