92446afb6eed9ea80bb76024edb312fdd27b66df
Sanctus-Peter <sanctuspeter@gmail.com>
2022-08-10 03:36:26 +0100

    minor chamges

--- 5-printf.c
+++ 5-printf.c
@@ -18,6 +18,8 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (format == NULL)
 		return (-1);
+	if (!format[0])
+		return (0);
 	if (format[0] == '%' && _strlen(format) == 1)
 		return (-1);
 	val = _vprintf(format, args);
